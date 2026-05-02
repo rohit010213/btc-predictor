9e3ccbef6c0847e76d4c1f4925f865e685f373e3
Rohit Joshi <rohitjoshi@Rohits-MacBook-Air.local>
2026-05-03 03:48:23 +0530

    Add project files

--- backend/index.js
+++ backend/index.js
@@ -34,5 +34,5 @@ app.get('/health', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
